8fee0c59fd7f472343bafebb492a14859742e5b9
norb_gf <norb_gf@yahoo.com.br>
2021-06-14 20:19:04 -0300

    Initial cap1 ao cap5

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import Products from "./components/Products";
+import { Button } from 'react-bootstrap'
+import Rating from './components/Rating'
+import JumboTronComponent from './components/JumboTronComponent'
+
+class App extends Component {
+  render() {
+
+  // const IsValid = true;
+  const IsValid = false;
+
+    return (
+      <div className="App">
+        <JumboTronComponent>
+          This is a long text that will extends more than a few steps. A people, a lot of people
+          that have sleep yesterday
+        </JumboTronComponent> />
+        <Products />
+        <Rating rating="1" />
+        <Rating rating="5" />
+        <Rating rating="3" />
+        <Rating rating="2" />
+        <Rating rating="4" />
+        <Button variant='primary' disabled={!IsValid}>Default</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
